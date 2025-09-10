cc67a21d5ce44fddbb609c257344b2ba07a6468a
jepsss06 <jepisutarlan2@gmail.com>
2025-09-10 11:59:00 +0700

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(SecRoleSeeder::class);
-        $this->call(ProSeeder::class);
         $this->call(ProductcategorySeeder::class);
+        $this->call(ProSeeder::class);
     }
 }
